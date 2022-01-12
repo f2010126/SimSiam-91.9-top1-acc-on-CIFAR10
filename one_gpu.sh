dec7a3e9e2c5cf0c0eb21fc677caee478314a99a
Dipti Sengupta <diptisengupa@Diptis-MacBook-Pro-2.local>
2022-01-13 00:52:16 +0530

    Run with same args as repo

--- one_gpu.sh
+++ one_gpu.sh
@@ -2,8 +2,8 @@
 #SBATCH -p mlhiwidlc_gpu-rtx2080
 #SBATCH -q dlc-dsengupt
 #SBATCH --gres=gpu:1
-#SBATCH -o /work/dlclarge1/dsengupt-lth_ws/slurm_logs/one_gpu_simsiam.out # STDOUT  (the folder log has to be created prior to running or this won't work)
-#SBATCH -e /work/dlclarge1/dsengupt-lth_ws/slurm_logs/onegpu_simsiam.err # STDERR  (the folder log has to be created prior to running or this won't work)
+#SBATCH -o /work/dlclarge1/dsengupt-lth_ws/slurm_logs/800_1gpu_simsiam.out # STDOUT  (the folder log has to be created prior to running or this won't work)
+#SBATCH -e /work/dlclarge1/dsengupt-lth_ws/slurm_logs/800_1gpu_simsiam.err # STDERR  (the folder log has to be created prior to running or this won't work)
 #SBATCH -J One_GPU_SimSiam_Cifar10
 #SBATCH -t 19:59:00
 #SBATCH --mail-type=BEGIN,END,FAIL
@@ -16,6 +16,6 @@ pip list
 python3 -c "import torch; print(torch.__version__)"
 python3 -c "import torch; print(torch.cuda.is_available())"
 cd MetaSSL/SimSiam-91.9-top1-acc-on-CIFAR10
-echo "Pretrain Simsiam with CIFAR10 LR =0.03"
-python3 -m main --arch resnet18 --learning_rate 0.03 --epochs 100 --weight_decay 5e-4 --momentum 0.9 --batch_size 512 --gpu 0 --trial 'one_run'
+echo "Pretrain Simsiam with CIFAR10 LR =0.06"
+python3 -m main --arch resnet18 --learning_rate 0.06 --epochs 800 --weight_decay 5e-4 --momentum 0.9 --batch_size 512 --gpu 0 --trial '800_run_same_param'
 deactivate
