952ca25fbaf6a01174f0391785bfb9e621f1111a
wagnerd <wagnerd@informatik.uni-freiburg.de>
2022-01-14 16:09:31 +0100

    Update submission scripts

--- cluster/submit_master_simsiam_cifar10.sh
+++ cluster/submit_master_simsiam_cifar10.sh
@@ -4,9 +4,9 @@
 #SBATCH --gres=gpu:1
 ##SBATCH -o /work/dlclarge1/dsengupt-lth_ws/slurm_logs/800_1gpu_simsiam.out # STDOUT  (the folder log has to be created prior to running or this won't work)
 ##SBATCH -e /work/dlclarge1/dsengupt-lth_ws/slurm_logs/800_1gpu_simsiam.err # STDERR  (the folder log has to be created prior to running or this won't work)
-#SBATCH -J ERC_MSSL_Cifar10_SimSiam_FT
-#SBATCH -t 19:59:00
-#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH -J ERC_MSSL_Cifar10_SimSiam
+#SBATCH -t 3-00:00 # time (D-HH:MM)
+##SBATCH --mail-type=BEGIN,END,FAIL
 
 # cd $(ws_find lth_ws)
 # python3 -m venv lth_env
@@ -18,9 +18,8 @@ python3 -c "import torch; print(torch.cuda.is_available())"
 
 # cd MetaSSL/SimSiam-91.9-top1-acc-on-CIFAR10
 
-echo "FineTune Simsiam with CIFAR10 LR =0.06"
+echo "Pretrain Simsiam with CIFAR10 LR =0.06"
 
-python -m both --trial $EXPERIMENT_NAME --seed 3 --pretrained /work/dlclarge2/wagnerd-metassl_experiments/CIFAR10/$EXPERIMENT_NAME/$EXPERIMENT_NAME --ft_learning_rate 30 --gpu 0
-
+python3 -m both --gpu 0 --is_bohb_run --valid_size 0.1 --run_background_worker --seed 1 --trial $EXPERIMENT_NAME --exp_dir "/work/dlclarge2/wagnerd-metassl_experiments/BOHB/CIFAR10" --pretrained /work/dlclarge2/wagnerd-metassl_experiments/BOHB/CIFAR10/$EXPERIMENT_NAME/$EXPERIMENT_NAME --n_iterations 250 --run_id "color-jitter"
 
 # deactivate
--- cluster/submit_worker_simsiam_cifar10.sh
+++ cluster/submit_worker_simsiam_cifar10.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH -p mlhiwidlc_gpu-rtx2080
-#SBATCH -q dlc-wagnerd
+#SBATCH -p bosch_gpu-rtx2080
+##SBATCH -q dlc-wagnerd
 #SBATCH --gres=gpu:1
 ##SBATCH -o /work/dlclarge1/dsengupt-lth_ws/slurm_logs/800_1gpu_simsiam.out # STDOUT  (the folder log has to be created prior to running or this won't work)
 ##SBATCH -e /work/dlclarge1/dsengupt-lth_ws/slurm_logs/800_1gpu_simsiam.err # STDERR  (the folder log has to be created prior to running or this won't work)
 #SBATCH -J ERC_MSSL_Cifar10_SimSiam
-#SBATCH -t 19:59:00
-#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH -t 3-00:00 # time (D-HH:MM)
+##SBATCH --mail-type=BEGIN,END,FAIL
 
 # cd $(ws_find lth_ws)
 # python3 -m venv lth_env
@@ -20,6 +20,6 @@ python3 -c "import torch; print(torch.cuda.is_available())"
 
 echo "Pretrain Simsiam with CIFAR10 LR =0.06"
 
-python3 -m main --is_bohb_run --seed 3 --arch resnet18 --learning_rate 0.06 --epochs 5 --weight_decay 5e-4 --momentum 0.9 --batch_size 512 --gpu 0 --trial $EXPERIMENT_NAME --exp_dir "/work/dlclarge2/wagnerd-metassl_experiments/CIFAR10"
+python3 -m both --gpu 0 --is_bohb_run --valid_size 0.1 --run_background_worker --seed 1 --trial $EXPERIMENT_NAME --exp_dir "/work/dlclarge2/wagnerd-metassl_experiments/BOHB/CIFAR10" --pretrained /work/dlclarge2/wagnerd-metassl_experiments/BOHB/CIFAR10/$EXPERIMENT_NAME/$EXPERIMENT_NAME --n_iterations 250 --run_id "color-jitter" --worker
 
 # deactivate
